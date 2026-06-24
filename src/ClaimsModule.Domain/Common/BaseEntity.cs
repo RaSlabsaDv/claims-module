ea3e5ac90b1b7767c7a279329bb4c8241acdb3fa
Maksym <monctrikua@gmail.com>
2026-06-24 12:29:16 +0300

    feat: add BaseEntity

---
+++ src/ClaimsModule.Domain/Common/BaseEntity.cs
@@ -0,0 +1,35 @@
+namespace ClaimsModule.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; }
+
+    public DateTimeOffset CreatedAt { get; private set; }
+    public DateTimeOffset? UpdatedAt { get; private set; }
+    public Guid? UserCreated { get; private set; }
+    public Guid? UserModified { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+    public DateTimeOffset? DeletedAt { get; private set; }
+
+    protected BaseEntity() { }
+
+    protected void SetCreated(Guid userId)
+    {
+        CreatedAt = DateTimeOffset.UtcNow;
+        UserCreated = userId;
+    }
+
+    protected void SetUpdated(Guid userId)
+    {
+        UpdatedAt = DateTimeOffset.UtcNow;
+        UserModified = userId;
+    }
+
+    public void SoftDelete(Guid userId)
+    {
+        IsDeleted = true;
+        DeletedAt = DateTimeOffset.UtcNow;
+        SetUpdated(userId);
+    }
+}
