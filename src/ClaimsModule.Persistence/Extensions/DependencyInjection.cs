3d59817420824c3cc7564abfe611bb21411d4b91
Maksym <monctrikua@gmail.com>
2026-06-27 00:50:19 +0300

    feat: add Persistence DependencyInjection

---
+++ src/ClaimsModule.Persistence/Extensions/DependencyInjection.cs
@@ -0,0 +1,40 @@
+using ClaimsModule.Application.Common.Interfaces;
+using ClaimsModule.Persistence.Repositories;
+using ClaimsModule.Persistence.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClaimsModule.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ClaimsDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"),
+                sql => sql.MigrationsAssembly(typeof(ClaimsDbContext).Assembly.FullName)));
+
+        // UoW
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        // Repositories
+        services.AddScoped<IClaimRepository, ClaimRepository>();
+        services.AddScoped<IReserveRepository, ReserveRepository>();
+        services.AddScoped<IClaimPartyRepository, ClaimPartyRepository>();
+        services.AddScoped<IClaimRiskObjectRepository, ClaimRiskObjectRepository>();
+        services.AddScoped<IClaimDocumentRepository, ClaimDocumentRepository>();
+        services.AddScoped<IClaimAuditLogRepository, ClaimAuditLogRepository>();
+        services.AddScoped<IClaimAuditLogWriter, ClaimAuditLogWriter>();
+        services.AddScoped<IPolicyRepository, PolicyRepository>();
+        services.AddScoped<ICauseOfLossCodeRepository, CauseOfLossCodeRepository>();
+
+        // Services
+        services.AddScoped<IClaimNumberGenerator, ClaimNumberGenerator>();
+
+        return services;
+    }
+}
