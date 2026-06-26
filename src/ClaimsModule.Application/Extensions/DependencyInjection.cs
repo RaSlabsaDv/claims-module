ac754c6645915f34f612c82812bcf39a8b1755ec
Maksym <monctrikua@gmail.com>
2026-06-27 00:36:19 +0300

    feat: add Application DependencyInjection with MediatR, AutoMapper and FluentValidation

---
+++ src/ClaimsModule.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using ClaimsModule.Application.Common.Behaviors;
+using ClaimsModule.Application.Common.Interfaces;
+using ClaimsModule.Application.Services;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClaimsModule.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(cfg => 
+        {
+            cfg.AddMaps(Assembly.GetExecutingAssembly());
+        });
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(TransactionBehavior<,>));
+        });
+
+        services.AddScoped<IAuditLogService, AuditLogService>();
+
+        return services;
+    }
+}
